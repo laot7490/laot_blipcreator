139904d6c9edec6925f6f0acfd9d9d95d4cf78cd
Emir <70145288+laot7490@users.noreply.github.com>
2020-08-24 17:16:26 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Blips = {
+    ["blip1"] = {
+        title="Laot 1", colour=5, id=446, x = -347.291, y = -133.370, z = 38.009
+    },
+    ["blip2"] = {
+        title="Laot 2", colour=3, id=446, x = -204.4, y = -1306.58, z = 31.31
+    },
+}
